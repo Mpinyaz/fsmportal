a6b1f88c2b60c68bb103837f434e9bce9dd2ca5a
Mpinyaz <ebmpinyuri@gmail.com>
2024-11-07 10:18:47 +0200

    added statemachine impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-
+use std::fmt::Debug;
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum CallState {
     Idle,
@@ -31,11 +31,28 @@ pub struct StateMachine {
     context: HashMap<String, usize>,
 }
 
+pub enum Response<S> {
+    Handled,
+    Transition(S),
+}
+pub trait StateHandler<S, CTX, E: Debug> {
+    fn on_event(&mut self, event: &E, context: &mut CTX) -> Response<S>;
+    fn on_exit(&mut self, context: &mut CTX);
+}
+
 impl StateMachine {
-    pub fn new() -> Self {
+    pub fn new(context: HashMap<String, usize>) -> Self {
         StateMachine {
-            current_state: CallState::Idle, // Default state
-            context: HashMap::new(),        // Empty HashMap context
+            current_state: CallState::Idle,
+            context,
         }
+    }
+
+    fn transition(&mut self, next_state: CallState) {
+        println!(
+            "Transitioning from {:?} to {:?}",
+            self.current_state, next_state
+        );
+        self.current_state = next_state;
     }
 }
